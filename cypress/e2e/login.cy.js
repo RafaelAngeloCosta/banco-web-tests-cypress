136e9650325bd27e23e13221d5214eb983fbf701
@RafaelAngeloCosta <rafa.angelo9@gmail.com>
2025-11-09 12:22:07 -0300

    Adicionei um teste para validar login invalidos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,9 +6,28 @@ describe('Login', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    // cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.contains('h4','Realizar Transferência').should('be.visible')
   })
+
+  it.only('Login com dados Inválidos deve apresentar mensagem de erro', () => {
+    //Arrange
+    cy.visit('http://localhost:4000/')
+
+    //Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('457854')
+    // cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
+
+    //Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+
+
+  })  
+
+
 })
